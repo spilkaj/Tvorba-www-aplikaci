fc2142ee3a6ec3512045146079ed8c056a0b6175
spilkaj <honza.speaky@gmail.com>
2021-02-11 16:10:55 +0100

    ram.php

--- ram.php
+++ ram.php
@@ -1,7 +1,26 @@
-<?php include 'includes/header.php';?>
-        
-        <h1>Operační paměti</h1>
+<?php include 'includes/header.php'; ?>
 
-        <?php include 'includes/footer.php';?>
-    <body>
-</html>
+<h1>Operační paměti</h1>
+<div class="grid-container-text">
+    <div class="text">
+        <p>Operační paměť je tvořena jednotlivými paměťovými buňkami, uspořádanými do matice. Buňky jsou realizovány rozličnými technologiemi. Dohromady tvoří paměťové čipy, které jsou osazeny na paměťovém modulu. Tyto moduly pak instalujeme na základní desku počítače. Jednotlivé moduly se od sebe liší velikostí paměti, rychlostí a také tvarem. Abychom mohli určitý typ modulu operační paměti používat, musí být základní deska vybavena příslušným slotem a musí daný typ paměti podporovat.
+        </p><br>
+        <p>Procesor využívá paměť RAM jako úložný prostor pro data, výsledky výpočtů a programových instrukcí a toto "skladiště" používá podle potřeby k provádění úloh vyžadovaných programy. Pro ukládání dat a jejich vybírání CPU udává paměťovou adresu požadované informace. Adresová sběrnice umožňuje procesoru poslat adresu do RAM a datová sběrnice má na starost vlastní přenos dat do CPU. Samotný termín sběrnice označuje spojení mezi dvěma zařízeními, jež jim umožňuje vzájemně komunikovat.
+        </p><br>
+        <p>Kritériem výkonu paměti RAM je přístupová doba, tedy čas, který uplyne mezi okamžikem, kdy CPU vydá paměti RAM instrukci k přečtení určitých dat z určité adresy a okamžikem, kdy CPU data přijme.
+        </p><br>
+        <p>Paměť cache je mnohem rychlejší než hlavní paměť, ale systém jí má mnohem méně (cache paměť je drahá) a proto se do ní umísťují pouze vybraná data, která CPU bude pravděpodobně potřebovat v následujícím okamžiku. Tento výběr obstarává řadič cache paměti.
+        </p><br>
+        <p>Dalším důležitým parametrem paměti je její velikost. Velikost se udává v gigabajtech (GB). Čím větší je velikost operační paměti, tím více se do ní vejde informací. Běžné počítače obsahují 4 až 8 GB paměti, výkonnější PC ještě více.
+        </p>
+    </div>
+    <div class="picture">
+        <img src="pictures/ram.png" alt="Operační paměť">
+    </div>
+</div>
+
+<?php include 'includes/footer.php'; ?>
+
+<body>
+
+    </html>
