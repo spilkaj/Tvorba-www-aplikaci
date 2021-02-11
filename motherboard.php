fcd8822d306b67a08f092bdf03f1a15b882ee004
spilkaj <honza.speaky@gmail.com>
2021-02-11 15:31:32 +0100

    motherboard.php

--- motherboard.php
+++ motherboard.php
@@ -1,7 +1,21 @@
-<?php include 'includes/header.php';?>
-        
-        <h1>Grafiká karta</h1>
+<?php include 'includes/header.php'; ?>
 
-        <?php include 'includes/footer.php';?>
+    <h1>Základní deska</h1>
+
+    <div class="grid-container-text">
+        <div class="text">
+            <p>Základní deska je základním stavebním kamenem osobního počítače, její vlastnosti určují do značné míry vlastnosti celého počítače. Základní deska (také mateřská deska nebo motherboard) plní v počítači mnoho úkolů.
+            </p><br>
+            <p>Do základní desky se instaluje procesor, paměti a rozšiřující karty, připojují se k ní také mechaniky, pevné disky, SSD a další zařízení. Základní desky se od sebe liší například svou velikostí (ATX, mATX) nebo tím, jaké procesory a paměti se do nich mohou instalovat, jaké mají uspořádání či integrované komponenty.
+            </p>
+        </div>
+        <div class="picture">
+            <img src="pictures/motherboard.png" alt="Motherboard">
+        </div>
+    </div>
+
+    <?php include 'includes/footer.php'; ?>
+
     <body>
+
 </html>
