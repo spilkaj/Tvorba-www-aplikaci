9bb87a2365d9b5690d730ad3f1420f63d5738c3d
spilkaj <honza.speaky@gmail.com>
2021-02-11 15:17:29 +0100

    procesor.php progress

--- procesor.php
+++ procesor.php
@@ -1,7 +1,26 @@
-<?php include 'includes/header.php';?>
-        
-        <h1>Procesor</h1>
+<?php include 'includes/header.php'; ?>
 
-        <?php include 'includes/footer.php';?>
-    <body>
-</html>
+<h1>Procesor</h1>
+<div class="grid-container-text">
+    <div class="text"><p>Procesor můžeme označit jako hnací motor celého počítače. Na starosti má provádění veškerých programových instrukcí, zajišťuje spolupráci všech periferií a provádí matematické operace.
+    </p><br>
+    <p>Fyzicky je procesor realizován jako velmi složitý integrovaný obvod s mnoha vývody. Existují dvě varianty procesorů - procesory určené pro patice Slot a procesory určené pro patice Socket. Procesory do Slotu mají tvar přídavné karty s vývody uspořádanými do jedné řady. Toto řešení používaly starší procesory Intel Pentium III a Intel Celeron. Procesory určené pro patici Socket mají tvar čtverce, po jehož obvodu jsou umístěny v několika řadách vývody. Patice pro tento druh procesoru disponuje speciálním upínacím mechanismem. Díky němu je možná snadná instalace a odebrání procesoru. Dnes se prakticky vyskytují jen procesory s paticí Socket.
+    </p><br>
+    <p>Procesor se při práci zahřívá a proto je nutné jej chladit. K tomu používáme různé typy chladičů a větráčků. U některých druhů lze regulovat a snímat počet otáček.
+    </p><br>
+    <p>Základním parametrem procesoru je bezesporu jeho výkon. Ten je ovlivněn mnoha činiteli, z nichž nejvýznamnější je pracovní frekvence. Udává se v MHz. Tato hodnota značí, kolik instrukčních cyklů je procesor schopen vykonat za jednu vteřinu. Je užitečné poznamenat, že jeden instrukční cyklus není shodný s instrukcí. Naprostá většina instrukcí je příliš složitá na to, aby mohla být vykonána v jednom instrukčním cyklu a proto je rozložena na dílčí operace. Tyto operace jsou již natolik jednoduché, že mohou být provedeny v jediném instrukčním cyklu. Například instrukce sčítání dvou čísel zabere 4 cykly. Dnešní nejnovější procesory pracují na frekvencích kolem 2 GHz.
+    </p><br>
+    <p>Dalšími faktory ovlivňujícími výkon procesoru je například velikost primární a sekundární paměti cache, nebo počet aritmeticko-logických jednotek vykonávajících matematické operace.
+    </p>
+    </div>
+    <div class="picture">
+        <img src="pictures/procesor1.png" alt="Procesor">
+        <img src="pictures/procesor2.png" alt="Procesor">
+    </div>
+</div>
+
+<?php include 'includes/footer.php'; ?>
+
+<body>
+
+    </html>
