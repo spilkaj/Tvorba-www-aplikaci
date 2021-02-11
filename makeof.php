570b34baefce6ade62515f5a0e1545d3ce960b92
spilkaj <honza.speaky@gmail.com>
2021-02-11 17:31:47 +0100

    makeof.php

--- makeof.php
+++ makeof.php
@@ -1,7 +1,34 @@
-<?php include 'includes/header.php';?>
-        
-        <h1>Jak film vznikl</h1>
+<?php include 'includes/header.php'; ?>
 
-        <?php include 'includes/footer.php';?>
-    <body>
-</html>
+<h1>Jak film vznikl</h1>
+<div class="grid-container-text">
+    <div class="text">
+        <p><b>Projekt vznikl a pedagogické fakulté UK pro vzdělávací účely, katedře informačních technologií a technické výchovy.</b> 
+        </p><br>
+        <p>Cílem projektu bylo vytvořit webové rozhraní na vybrané téma.<br>
+        Tento projekt má naučit studenty, jak fyzicky sestrojit stolní počítač a zároveň se dozvědět něco navíc o komponentech.
+        </p><br>
+        <p>Natáčení probíhalo přibližně 14 hodin, včetně příprav na nátáčení a úprav scény v jeho průběhu. Následný střih a úprava filmu, včetně dabingu a úpravy zvuku trval přibližně 16 hodin.            
+        </p>
+        <br><br><br>
+        <p><b>Pro natáčení byla použita tato technika:</b></p><br>
+        <p>
+            Canon EOS 6D mark II<br>
+            Objektiv Canon EF 50mm f/1.8 STM<br>
+            Softbox<br>
+            2 další bodová osvětlení
+        </p>
+    </div>
+    <div class="picture">
+        <img src="pictures/canonTelo.png" alt="Canon EOS 6D mark II">
+        <img src="pictures/objektiv.png" alt="Objektiv Canon EF 50mm f/1.8 STM">
+        <img src="pictures/softbox.png" alt="softbox">
+
+    </div>
+</div>
+
+<?php include 'includes/footer.php'; ?>
+
+<body>
+
+    </html>
